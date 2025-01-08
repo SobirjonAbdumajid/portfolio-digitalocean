954cab6c7c1af8b53ebd8ed3b8b0c0bd6ed6dcd0
Sobirjon005 <sobirjon0305@gmail.com>
2025-01-08 16:31:04 +0500

    d

--- Config/urls.py
+++ Config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hide_admin1/', admin.site.urls),
     path('', include('myApp.urls')),
     path('', include('account.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
