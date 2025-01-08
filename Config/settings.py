50247d9095c88bb5e1355f8488d41f6535a55ffe
Sobirjon005 <sobirjon0305@gmail.com>
2025-01-08 15:34:23 +0500

    changes

--- Config/settings.py
+++ Config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3sj5+8o=))ak)@h6-*wg@8_&+=&y98&p98uol#(96q_$sl!$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio.sobirjon.codes', '127.0.0.1', '167.99.140.20']
 
